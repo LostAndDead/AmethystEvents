1254c6b9f5d7c3dc41b9dc31f6dc3f98b3d75b3c
Poulette <63922760+PouletteMC@users.noreply.github.com>
2021-05-03 11:41:35 +0200

    Guardian hits give 10xp points (amount tbd)

--- src/main/java/co/uk/lostanddead/amethystevents/Events/TheGuardianExperience.java
+++ src/main/java/co/uk/lostanddead/amethystevents/Events/TheGuardianExperience.java
@@ -2,7 +2,11 @@ package co.uk.lostanddead.amethystevents.Events;
 
 import co.uk.lostanddead.amethystevents.AmethystEvents;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Guardian;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 public class TheGuardianExperience implements Listener {
 
@@ -12,7 +16,7 @@ public class TheGuardianExperience implements Listener {
         this.core = core;
     }
 
-    public String getName(){
+    public String getName() {
         return ChatColor.GREEN + "The Guardian Experience";
     }
 
@@ -20,5 +24,15 @@ public class TheGuardianExperience implements Listener {
         return "The guardians are just misunderstood";
     }
 
+    @EventHandler
 
+    public void EntityDamageByEntityEvent(EntityDamageByEntityEvent event) {
+
+        if (event.getDamager() instanceof Guardian && event.getEntity() instanceof Player) {
+             Player p = (Player) event.getEntity();
+             p.giveExp(10);
+        }
+    }
 }
+
+
