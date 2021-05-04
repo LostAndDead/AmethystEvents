dc1764c23296bb2c6e1cee2ca31f40846a881ffa
Poulette <63922760+PouletteMC@users.noreply.github.com>
2021-05-04 09:11:59 +0200

    New Event "The Guardian Experience"

---
+++ src/main/java/co/uk/lostanddead/amethystevents/Events/TheGuardianExperience.java
@@ -0,0 +1,33 @@
+package co.uk.lostanddead.amethystevents.Events;
+
+import co.uk.lostanddead.amethystevents.AmethystEvents;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Guardian;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+public class TheGuardianExperience implements Listener {
+    public AmethystEvents core;
+
+    public TheGuardianExperience(AmethystEvents core) {
+        this.core = core;
+    }
+
+    public String getName() {
+        return ChatColor.GREEN + "The Guardian Experience";
+    }
+
+    public String getDescription() {
+        return "The Guardians are just misunderstood";
+    }
+
+    @EventHandler
+    public void EntityDamageByEntityEvent(EntityDamageByEntityEvent event) {
+        if (event.getDamager() instanceof Guardian && event.getEntity() instanceof Player) {
+            Player p = (Player) event.getEntity();
+            p.giveExp(2);
+            }
+        }
+    }
